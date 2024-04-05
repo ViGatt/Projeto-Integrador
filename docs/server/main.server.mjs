7853e334a65922e2417e14793320d18bdc3e35a5
Victor Gatti <viniciusgattirodrigues2003@gmail.com>
2024-04-05 14:22:26 -0300

    ajustes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BEB3BH5A.mjs";import"./chunk-34AMI75P.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-W3GU6DUY.mjs";import"./chunk-34AMI75P.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
