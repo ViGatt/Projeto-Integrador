e8057c4142c6c46a4a64fb58a434c793454d23fa
Victor Gatti <viniciusgattirodrigues2003@gmail.com>
2024-05-12 19:55:11 -0300

    'projeto'

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YK3DKCBM.mjs";import"./chunk-34AMI75P.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-6KBAAWSO.mjs";import"./chunk-34AMI75P.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
